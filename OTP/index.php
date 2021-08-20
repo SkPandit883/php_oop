cce22d071908a59ded68e0606f2141147ba19945
santosh kumar pandit <skpandit659@gmail.com>
2021-08-20 12:52:56 +0545

    fix otp

--- OTP/index.php
+++ OTP/index.php
@@ -1,9 +1,13 @@
 <?php
 namespace OTP;
-
 class OTP{
     public static function generateOTP($digits){
-        return $digits;
+        $generator = "1357902468";
+        $otp = "";
+        for ($i = 1; $i <= $digits; $i++) {
+            $otp .= substr($generator, (rand()%(strlen($generator))), 1);
+        }
+        return $otp;
     }
 }
 
