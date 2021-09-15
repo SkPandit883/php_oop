8951cc9449af9091dd1c3c2bed2e7df95e2627ee
santosh kumar pandit <skpandit659@gmail.com>
2021-09-15 19:53:09 +0545

    function

--- MagicConstant/index.php
+++ MagicConstant/index.php
@@ -2,5 +2,10 @@
 
 echo "line no: ".__LINE__."<br>";
 echo "file path: ".__FILE__."<br>";//return path of file
-echo "file dir: ".__DIR__;//return path of file
+echo "file dir: ".__DIR__."<br>";//return path of file
+
+function MyFunction(){
+    echo "function name :".__FUNCTION__."<br>";
+}
+MyFunction()
 ?>
